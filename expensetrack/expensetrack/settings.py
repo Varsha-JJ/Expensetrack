3eb502da8f84d4f6f2008f1dc65b45d450735157
Varsha <Varshajj75@gmail.com>
2023-10-07 12:29:01 +0530

    hidded

--- expensetrack/expensetrack/settings.py
+++ expensetrack/expensetrack/settings.py
@@ -129,12 +129,6 @@ STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_HOST_USER = 'onlinefirststep1@gmail.com'
-# DEFAULT_FROM_EMAIL = 'onlinefirststep1@gmail.com'
-# SERVER_EMAIL = 'onlinefirststep1@gmail.com'
-# EMAIL_HOST_PASSWORD = 'whvp jyuu pzdv juwa'
+
 
 EMAIL_USE_TLS = True
